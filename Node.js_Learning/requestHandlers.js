f48e66a88a088ce6523da94f72df85922b19461e
Yu-Ting, Kao <jackzzjack@gmail.com>
2015-06-10 16:29:55 +0000

    Changelogs: To  highlight the non-blocking mode, we ㄍexpand the execution loading time.

--- Node.js_Learning/requestHandlers.js
+++ Node.js_Learning/requestHandlers.js
@@ -3,14 +3,22 @@ var exec = require("child_process").exec;
 function start(response) {
     console.log("Request Handler 'start' was called.");
     
-    exec("ls -lah", function(error, stdout, stderr) {
+    /*exec("ls -lah", function(error, stdout, stderr) {
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write(stdout);
         
         //response.write("This is start page.\n");
     
         response.end();
-    });
+    });*/
+    
+    exec("find /", { timeout: 10000, maxBuffer: 10000*1024 }
+        , function(error, stdout, stderr) {
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            response.write(stdout);
+            
+            response.end();
+        });
 }
 
 function upload(response) {
