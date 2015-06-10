c992b1572d6560a70e2587db1a11b2090f61aafa
Yu-Ting <yutingkao23@gmail.com>
2015-06-10 03:14:22 +0000

    Chagnelogs: Modify to suit Cloud9.

--- Node.js_Learning/server.js
+++ Node.js_Learning/server.js
@@ -15,7 +15,7 @@ function start(router, handle) {
         response.end();
     }
     
-    http.createServer(onRequest).listen(8888);
+    http.createServer(onRequest).listen(process.env.PORT, process.env.IP);
     console.log("On Listening");
 }
 
