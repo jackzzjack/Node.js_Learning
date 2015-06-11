970482ae63a8c3dcc68a6e5572b030479b5a8f0c
Yu-Ting, Kao <jackzzjack@gmail.com>
2015-06-11 02:53:03 +0000

    Changelogs: Add post method with textarea in index page.

--- Node.js_Learning/requestHandlers.js
+++ Node.js_Learning/requestHandlers.js
@@ -12,13 +12,30 @@ function start(response) {
         response.end();
     });*/
     
-    exec("find /", { timeout: 10000, maxBuffer: 10000*1024 }
+    /*exec("find /", { timeout: 10000, maxBuffer: 10000*1024 }
         , function(error, stdout, stderr) {
             response.writeHead(200, {"Content-Type": "text/plain"});
             response.write(stdout);
             
             response.end();
-        });
+        });*/
+        
+    var body = '<html>' +
+                '<head>' +
+                '<meta http-equiv="Content-Type" content="text/html; ' +
+                'charset=UTF-8" />' +
+                '</head>' +
+                '<body>' +
+                '<form action="/upload" method="post">' +
+                '<textarea name="text" rows="20" cols="60"></textarea>' +
+                '<input type="submit" value="Submit text" />' +
+                '</form>' +
+                '</body>' +
+                '</html>';
+                
+    response.writeHead(200, {"Content-Type" : "text/html"});
+    response.write(body);
+    response.end();
 }
 
 function upload(response) {
