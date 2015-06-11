7b1765bd59f88132b6f29372f1effe327deb01e8
Yu-Ting, Kao <jackzzjack@gmail.com>
2015-06-11 03:24:50 +0000

    Changelogs: Add request listener to get the post data. (non-blocking mode)

--- Node.js_Learning/server.js
+++ Node.js_Learning/server.js
@@ -3,11 +3,23 @@ var url = require("url");
 
 function start(router, handle) {
     function onRequest(request, response) {
+        var postData = "";
         var pathname = url.parse(request.url).pathname;
         
         //console.log("Request is " + pathname);
         
-        router.route(handle, pathname, response);
+        request.setEncoding("utf8");
+        
+        request.addListener("data", function(postDataChunk) {
+           postData = postData + postDataChunk;
+           console.log("Receive POST data chunk '" + postDataChunk + "'");
+        });
+        
+        request.addListener("end", function() {
+            router.route(handle, pathname, response, postData);
+        });
+        
+        //router.route(handle, pathname, response);
     }
     
     http.createServer(onRequest).listen(process.env.PORT, process.env.IP);
