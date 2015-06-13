d08ea86ba8896758753c959e13f21e28b7f75969
Yu-Ting, Kao <jackzzjack@gmail.com>
2015-06-13 05:39:27 +0000

    ChangeLogs: Modify to the final version with formidable.

--- Node.js_Learning/formidable_id/requestHandlers.js
+++ Node.js_Learning/formidable_id/requestHandlers.js
@@ -41,11 +41,18 @@ function start(response) {
                 '</html>';*/
                  // Normal Mode (Ready to Upload Page)
     var body = 
-        '<form action="/upload" enctype="multipart/form-data" method="post">' +
-        '<input type="text" name="title"><br>' +
-        '<input type="file" name="upload" multiple="multiple"><br>' +   // Need to check what is multiple attribute.
-        '<input type="submit" value="Upload">' +
-        '</form>';
+        '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" ' +
+        '</head>' +
+        
+        '<body>' +
+            '<form action="/upload" enctype="multipart/form-data" method="post">' +
+            '<input type="file" name="upload" /><br>' +   // Need to check what is multiple attribute.
+            '<input type="submit" value="Upload file" />' +
+            '</form>' +
+        '</body>' +
+        '</html>';
                 
     response.writeHead(200, {"Content-Type" : "text/html"});
     response.write(body);
@@ -55,7 +62,7 @@ function start(response) {
 function upload(response, request) {
     console.log("Request Handler 'upload' was called.");
     
-    var form = new formidable.IncomingForm();
+    /*var form = new formidable.IncomingForm();
     form.parse(request, function(error, fields, files) {
        console.log("parsing done.");
        
@@ -63,8 +70,9 @@ function upload(response, request) {
        response.writeHead(200, {"content-type" : 'text/html'});
        response.write('received image: <br />');
        response.write("<img src='/show' /");
+       
        resonse.end();
-    });
+    });*/
     
     //response.writeHead(200, {"Content-Type": "text/plain"});
     //response.write("You have send " + querystring.parse(postData).text);
@@ -72,6 +80,19 @@ function upload(response, request) {
     //response.write("This is Upload\n");
     
     //response.end();
+    
+    var form = new formidable.IncomingForm();
+    console.log("Ready to parse.");
+    
+    form.parse(request, function(error, fields, files) {
+        console.log("Parse done.");
+        
+        fs.renameSync(files.upload.path, "/tmp/test.png");
+        response.writeHead(200, { 'Content-Type' : "text/html"});
+        response.write("receive image.");
+        response.write('<img src="/show" />');
+        response.end();
+    });
 }
 
 function show(response) {
--- Node.js_Learning/formidable_id/server.js
+++ Node.js_Learning/formidable_id/server.js
@@ -37,23 +37,14 @@ var url = require("url");
 
 function start(router, handle) {
     function onRequest(request, response) {
-        var postData = "";
+        //var postData = "";
         var pathname = url.parse(request.url).pathname;
         
         //console.log("Request is " + pathname);
         
-        request.setEncoding("utf8");
+        //request.setEncoding("utf8");
         
-        request.addListener("data", function(postDataChunk) {
-           postData = postData + postDataChunk;
-           console.log("Receive POST data chunk '" + postDataChunk + "'");
-        });
-        
-        request.addListener("end", function() {
-            router.route(handle, pathname, response, postData);
-        });
-        
-        //router.route(handle, pathname, response);
+        router.route(handle, pathname, response, request);
     }
     
     http.createServer(onRequest).listen(process.env.PORT, process.env.IP);
