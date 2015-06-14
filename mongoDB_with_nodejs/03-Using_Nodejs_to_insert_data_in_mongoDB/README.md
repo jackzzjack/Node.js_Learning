dfcccdfa8a06d49eac6daf6793f4689832317118
Yu-Ting, Kao @ MAC <jackzzjack@gmail.com>
2015-06-14 13:39:05 +0800

    ChangeLogs: Add the inserted data into mongodb portion.

---
+++ mongoDB_with_nodejs/03-Using_Nodejs_to_insert_data_in_mongoDB/README.md
@@ -0,0 +1,13 @@
+# Insert the data into mongodb.
+
+## Before execute node file, you should use npm to install mongodb first.
+
+- Change to your connection file directory.
+
+- execute the following command.
+
+`npm install mongodb`
+
+## Execute the node.js file to insert the data into mongodb from node.js driver.
+ 
+`node interfere.js`
---
+++ mongoDB_with_nodejs/03-Using_Nodejs_to_insert_data_in_mongoDB/interfere.js
@@ -0,0 +1,42 @@
+var MongodbClient = require('mongodb');
+var assert = require('assert')
+var ObjectId = require('mongodb').objectId;
+var url = 'mongodb://localhost::27017/test';
+
+var insertDocument = function(db, callback) {
+	db.collection('restaurants').insertOne( {
+		"address" : {
+			"street" : "test st.",
+			"zipcode" : "12345",
+			"building" : "1265",
+			"coord" : [ -72.123, 12.4313 ]
+		},
+		"borwq": "swdwewq",
+		"city" : "taiwan",
+		"date" : [{
+				"date" : "2015/03/25",
+				"grade": "A",
+				"score": 23,
+			},{
+				"date" : "2015/08/18",
+				"grade": "A",
+				"score": 11
+			}
+		],
+		"name": "Jack",
+		"restaurants_id": "123121"
+	}, function(err, result) {
+		assert.equal(null, err);
+		console.log("Inserted a document into the restaurants collection.");
+
+	 // Call the callback function if it is existed.
+		callback(result);
+	});
+}
+
+MongodbClient.connect(url, function(err, db) {
+	assert.equal(null, err);
+	insertDocument(db, function() {
+		db.close();
+	});
+});
